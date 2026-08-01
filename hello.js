0e213a6c2c32558b59fc36207a0ffbd0b7a358fd
edwardcoded77 <eolagunju25@email.com>
2026-07-31 22:09:03 -0400

    Both files were updated!

--- hello.js
+++ hello.js
@@ -4,12 +4,15 @@ console.log(message);
 console.log("This JavaScript runs on my machine and i am loving coding experience.");
 
 
-
+/* Age Requirements */
 let age = prompt("How old are you ?");
 
-if ( age > 15 ){
-    console.log("You can watch the movies alone");
-}else{
-	console.log("Sorry! You cannot watch a movies");
+if ( age >= 18 ){
+    console.log("You can watch any movie!");
+ } else if (age >=16) {
+	console.log("You can watch the movie alone!.");
+} else {
+console.log("Sorry! You cannot watch the movie");
 }
-	
+
+
--- notes.md
+++ notes.md
@@ -5,3 +5,5 @@
 * Editor: VS Code
 * My name is Gbenga, and I am learning Git, Bash scripting, and software development.
 * I enjoy solving technical problems and building my skills through hands-on projects.
+* * Created another repository called text but it worked fine but prompted an error message,but my instructor came to rescue that the message was normal , since i had created repo without having created file before - rephrase professionally
+
